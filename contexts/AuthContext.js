602c2ae6042763ecd6d0b0f7687aa02d1de937f5
Abraham Paul <codexapp44@gmail.com>
2024-10-14 13:52:45 +0100

    Add .firebaserc and firebase.json configuration files for firebase hosting, Add a github actions workflow file to build and deploy to firebase hosting

--- contexts/AuthContext.js
+++ contexts/AuthContext.js
@@ -1,3 +1,4 @@
+"use client"
 import { createContext, useContext, useEffect, useState } from 'react';
 import { auth } from '@/lib/firebase';
 import { onAuthStateChanged } from 'firebase/auth';
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,9 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        'discord-dark': '#36393f',
+        'discord-light': '#dcddde',
+        'discord-blue': '#7289da',
       },
     },
   },
